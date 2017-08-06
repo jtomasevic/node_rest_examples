e461953668a6dae22fdce6e769f30bbc288a707a
Jovan <jtomasevic@gmail.com>
2017-08-06 23:36:27 +0200

    add packages for body parsing and one more get method for all roles

--- basic_example/app.js
+++ basic_example/app.js
@@ -4,12 +4,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var fs = require('fs')
+
+
 // use this to be able to parse json in advance. 
 // ... otherwise in request.body will not be available
 app.use(bodyParser.urlencoded({"extended":true}));
 app.use(bodyParser.json());
 
 var loginRouter = express.Router();
+var rolesRouter = express.Router();
 
 // define get route, request handler and send json response
 // this is just temp examples. next step is to build controllers and models.
@@ -25,10 +29,21 @@ loginRouter.route('/login')
     });
 
 
+rolesRouter.route('/roles')
+    .get(function (req, res){
+        fs.readFile('sample_data/roles.json', 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = JSON.parse(data);
+        res.json(data);
+        });
+    });
+
 
 // add previx api in front of route define by bookRouter
 app.use('/api', loginRouter);
-
+app.use('/api', rolesRouter);
 // just simple get definition. 
 app.get('/', function(req, res){
     res.send('welcome to my API!');
